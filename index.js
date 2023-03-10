03cd8a9a4242f5518f830251bade17817b346deb
Bamidele Odunlami <bamideleodunlami@Bamideles-MacBook-Pro.local>
2023-03-10 13:46:41 +0000

    First commit

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(express.json({ limit: "1mb" }));
 app.use((req, res, next)=>{
   res.set('Cache-Control', 'no-store')
   next();
+  console.log("cache cleared");
 });
 
 //access public diroctory and use static files (CSS JS )
